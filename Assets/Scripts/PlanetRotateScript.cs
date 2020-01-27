ea9894f345af817ed5c10007ed6621dc4c776a74
Ofek <ofekkm@gmail.com>
2020-01-27 23:06:24 +0200

    rotate planet

---
+++ Assets/Scripts/PlanetRotateScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotateScript : MonoBehaviour
+{
+public float rotateSpeed;
+    // Update is called once per frame
+ void rotatePlanet(GameObject player1){
+    float sensetiviyX = player1.GetComponent<firstPersonControl>().controllerSensitivityX;
+    float xAxis = tag == "MenuPlayer1" ? KeyJoyController.getXAxis_Player1() : KeyJoyController.getXAxis_Player2();
+    if (Mathf.Abs(xAxis) <= sensetiviyX)
+    {
+        xAxis = 0f;
+    }
+    else
+    {
+        transform.Rotate(Time.deltaTime * rotateSpeed * xAxis, 0, 0 );
+    }
+ }
+}
