40b3df518c2bced866863fc73dd6fa6c8edd85ec
Ofek <ofekkm@gmail.com>
2020-02-04 03:58:31 +0200

    lololol final

--- Assets/Scripts/TrailPieceBank.cs
+++ Assets/Scripts/TrailPieceBank.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class TrailPieceBank : MonoBehaviour
 {
 
-    public const int MAX_TRAIL_PIECES = 16672;
+    public const int MAX_TRAIL_PIECES = 4096;
     public const string TRAIL_PIECES_PARENT_NAME = "TrailPieces";
 
     [SerializeField] GameObject trailPiecePrefab;
